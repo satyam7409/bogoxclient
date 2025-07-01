da07f59e9194f69c981a4d0628791bdb01a65f0f
Satyam Bansal <satyambansal008@gmail.com>
2025-07-01 21:23:56 +0530

    fix initial log

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from "react";
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [isLogin, setIsLogin] = useState(true); // default: not logged in
+  const [isLogin, setIsLogin] = useState(false); // default: not logged in
 
   return (
     <UserContext.Provider value={{ isLogin, setIsLogin }}>
